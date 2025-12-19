ab6e918b59b6ea4635eb8f592328030e85f20637
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-20 00:42:32 +0700

    membuat Destinasihome

---
+++ app/src/main/java/com/example/per12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.per12.uicontroller.route
+
+import com.example.per12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
