82194222a37b7ab45c94db2af6d143357f7fbec1
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-20 00:43:59 +0700

    membuat HomeViewModel.kt

---
+++ app/src/main/java/com/example/per12/viewmodel/HomeViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.per12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.per12.modeldata.DataSiswa
+import com.example.per12.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+    init {
+        loadSiswa()
+    }
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
