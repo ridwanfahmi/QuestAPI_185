0b8302eb52fd133ddcfbd8ed3a5e6a5029e7e3de
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-20 00:44:24 +0700

    merevisi main activity

--- app/src/main/java/com/example/per12/MainActivity.kt
+++ app/src/main/java/com/example/per12/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.per12
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,21 +8,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.per12.ui.theme.Per12Theme
+import com.example.per12.ui.theme.per12Theme
+import com.example.per12.uicontroller.DataSiswaApp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Per12Theme {
+            per12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Per12Theme {
-        Greeting("Android")
-    }
-}
