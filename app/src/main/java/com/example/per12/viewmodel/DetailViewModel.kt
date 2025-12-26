2f4321a0c583415bba5351bee5749dc7f049574f
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:28:50 +0700

    Menambahkan StatusUIDetail sebagai sealed interface untuk merepresentasikan state loading, sukses, dan error pada detail siswa

--- app/src/main/java/com/example/per12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/per12/viewmodel/DetailViewModel.kt
@@ -1,2 +1,15 @@
+@file:OptIn(InternalSerializationApi::class)
 package com.example.per12.viewmodel
 
+
+
+import com.example.per12.modeldata.DataSiswa
+
+import kotlinx.serialization.InternalSerializationApi
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
