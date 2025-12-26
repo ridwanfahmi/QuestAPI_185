ccf230b6b6b1ddf2f9acf507a79e1a00c3168750
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:56:36 +0700

    Menambahkan komponen BarisDetailData untuk menampilkan pasangan label dan nilai data siswa dalam satu baris

--- app/src/main/java/com/example/per12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/per12/view/HalamanDetail.kt
@@ -3,7 +3,8 @@ package com.example.per12.view
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -30,7 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.per12.R
 import com.example.per12.modeldata.DataSiswa
@@ -197,7 +198,11 @@ fun DetailDataSiswa(
 private fun BarisDetailData(
     @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
 ) {
-
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
 }
 
 @Composable
