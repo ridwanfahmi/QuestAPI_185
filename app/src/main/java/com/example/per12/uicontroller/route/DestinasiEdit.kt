2319473e5c49b9a7815d75968634c975e3679ce9
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:15:43 +0700

    Menambahkan DestinasiDetail sebagai rute navigasi detail siswa dengan argumen ID

--- app/src/main/java/com/example/per12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/per12/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.per12.uicontroller.route
 
+import com.example.per12.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
