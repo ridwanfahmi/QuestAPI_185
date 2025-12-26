c53cbe4e7aa76c320eea31bcf294b6f3052734dc
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:12:02 +0700

    Menambahkan fungsi hapusSatuSiswa pada repository untuk menghapus data satu siswa berdasarkan ID melalui API

--- app/src/main/java/com/example/per12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/per12/repositori/RepositoryDataSiswa.kt
@@ -9,6 +9,7 @@ interface RepositoryDataSiswa{
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
     suspend fun getSatuSiswa(id:Int) : DataSiswa
     suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -18,4 +19,5 @@ class JaringanRepositoryDataSiswa(
     override suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
     override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id,dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
