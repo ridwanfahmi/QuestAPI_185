fc748bd2ab16881b057cc0b7bc3aeae510988ac4
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:18:26 +0700

    Menambahkan DestinasiEdit sebagai rute navigasi edit siswa dengan dukungan argumen ID

--- app/src/main/java/com/example/per12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/per12/uicontroller/route/DestinasiEdit.kt
@@ -2,9 +2,9 @@ package com.example.per12.uicontroller.route
 
 import com.example.per12.R
 
-object DestinasiDetail: DestinasiNavigasi {
-    override val route = "detail_siswa"
-    override val titleRes = R.string.detail_siswa
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
