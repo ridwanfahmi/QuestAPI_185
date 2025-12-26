d259c879fbdc3f031598bd81ed3f4736226c3bad
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:29:52 +0700

    Menambahkan DetailViewModel untuk mengambil data detail siswa dengan pengelolaan state UI (Loading, Success, Error)

--- app/src/main/java/com/example/per12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/per12/viewmodel/DetailViewModel.kt
@@ -3,13 +3,49 @@ package com.example.per12.viewmodel
 
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.per12.modeldata.DataSiswa
-
+import com.example.per12.repositori.RepositoryDataSiswa
+import com.example.per12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+import retrofit2.HttpException
 
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+
 }
