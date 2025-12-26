5652a85ecc542065e93d2c7680bd1f7aac46bb17
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:11:21 +0700

    Menambahkan fungsi getSatuSiswa pada repository untuk mengambil data satu siswa berdasarkan ID melalui API

--- app/src/main/java/com/example/per12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/per12/repositori/RepositoryDataSiswa.kt
@@ -7,6 +7,7 @@ interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
 
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
 }
 
 class JaringanRepositoryDataSiswa(
@@ -14,4 +15,5 @@ class JaringanRepositoryDataSiswa(
 ) : RepositoryDataSiswa{
     override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
