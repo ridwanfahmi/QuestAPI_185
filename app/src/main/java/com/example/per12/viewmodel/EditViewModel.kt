d997ba0450df9f5bb1f7fb60802d3a5221b4f021
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:24:07 +0700

    Menambahkan EditViewModel untuk mengambil dan mengelola data siswa berdasarkan ID menggunakan SavedStateHandle

--- app/src/main/java/com/example/per12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/per12/viewmodel/EditViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.per12.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+
+import com.example.per12.modeldata.UIStateSiswa
+import com.example.per12.modeldata.toUiStateSiswa
+import com.example.per12.repositori.RepositoryDataSiswa
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import com.example.per12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+
+}
