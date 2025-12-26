3405bb971e06eaa5eda5ccb0dc49aa5d72fd15c2
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-26 19:56:08 +0700

    Menambahkan dialog konfirmasi penghapusan data siswa menggunakan AlertDialog

--- app/src/main/java/com/example/per12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/per12/view/HalamanDetail.kt
@@ -3,12 +3,14 @@ package com.example.per12.view
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.per12.R
 import com.example.per12.modeldata.DataSiswa
@@ -43,7 +47,7 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
 
     Scaffold(
         topBar = {
@@ -91,16 +95,18 @@ fun DetailSiswaScreen(
         )
     }
 }
+
 @Composable
 private fun BodyDetailDataSiswa(
     statusUIDetail: StatusUIDetail,
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
+
     Column(
         modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
-    ){
+    ) {
 
         var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
 
@@ -110,6 +116,7 @@ private fun BodyDetailDataSiswa(
                 modifier = Modifier.fillMaxWidth())
             else -> {}
         }
+
         OutlinedButton(
             onClick = { deleteConfirmationRequired = true },
             shape = MaterialTheme.shapes.small,
@@ -130,10 +137,12 @@ private fun BodyDetailDataSiswa(
         }
     }
 }
+
 @Composable
 fun DetailDataSiswa(
     siswa: DataSiswa, modifier: Modifier = Modifier
 ) {
+
     Card(
         modifier = modifier, colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -184,15 +193,32 @@ fun DetailDataSiswa(
     }
 }
 
+@Composable
 private fun BarisDetailData(
     @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
-) {}
+) {
 
-
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCancel: () -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.tanya)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        })
+}
